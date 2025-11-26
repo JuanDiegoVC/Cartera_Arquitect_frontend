a121826c75715e0e20962a9b169e8c17f5698d8d
Juan Nuno <jsanchezn1519@gmail.com>
2025-11-26 00:49:00 -0500

    dev: funcionalidad de tunel ngrok en desarrollo parcial || Errores en transferencia de datos por parte de API

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite conexiones desde cualquier dirección
+    allowedHosts: true, // Permite el túnel de Ngrok (Vite 5.1+)
+  },
   resolve: {
     alias: {
       buffer: "buffer/",
